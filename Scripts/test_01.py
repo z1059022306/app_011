7acc18eecfa4a67d7d31d575005e6abb096c963e
zhang <993323162@qq.com>
2018-07-10 15:02:45 +0800

    as

--- Scripts/test_01.py
+++ Scripts/test_01.py
@@ -5,7 +5,7 @@ import allure
 class Test_01:
 
     with open('./phtot/text.png','rb') as f:
-        @allure.attach("sd",f,allure.attach.type.PNG)
+        @allure.attach("sd",f,allure.attach_type.PNG)
         @allure.step(title="第一个测试")
         def test_01(self):
             assert 0
