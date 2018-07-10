b250b11be7f723bf00af38c057b6ba6d3862650d
zhang <993323162@qq.com>
2018-07-10 15:11:53 +0800

    ss

--- Scripts/test_01.py
+++ Scripts/test_01.py
@@ -3,9 +3,12 @@ import allure
 
 
 class Test_01:
-        def test_01(self):
-            allure.step(title="第一个测试")
-            with open('./phtot/text.png', 'rb') as f:
-                allure.attach("sd", f.read(), allure.attach_type.PNG)
 
-            assert 0
+    def get_ph(self):
+        with open('./phtot/text.png', 'rb') as f:
+            allure.attach("sd", f.read(), allure.attach_type.PNG)
+    def test_01(self):
+        allure.step(title="第一个测试")
+
+
+        assert 0,self.get_ph()
